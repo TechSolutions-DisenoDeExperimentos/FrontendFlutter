45166624e6b2b1d357aec639b07767333e66484a
Rafael Primo <u202023137@upc.edu.pe>
2024-04-27 17:55:05 -0500

    Added version web without profile

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,42 @@
+import 'package:tu_cine_app/presentation/screens/auth/log_in.dart';
+import 'package:tu_cine_app/presentation/screens/cineclubs/cineclubs_screen.dart';
+import 'package:tu_cine_app/presentation/screens/movies/movie_tu_cine_screen.dart';
+import 'package:tu_cine_app/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: LoginPage.name,
+      builder: (context, state) => const LoginPage(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+        GoRoute(
+          path: 'movie-tu-cine/:id',
+          name: MovieTuCineScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieTuCineScreen(movieId: movieId);
+          },
+        ),
+        GoRoute(
+          path: 'cineclub/:id',
+          name: CineclubScreen.name,
+          builder: (context, state) {
+            final cineclubId = state.pathParameters['id'] ?? 'no-id';
+            return CineclubScreen(cineclubId: cineclubId);
+          },
+        ),
+      ],
+    ),
+  ],
+);
